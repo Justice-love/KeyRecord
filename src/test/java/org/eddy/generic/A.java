ed4fb97aea1feadc2bdc228914add3ef99106adc
eddy <eddyxu1213@126.com>
2017-02-26 15:47:07 +0800

    modify

--- src/test/java/org/eddy/generic/A.java
+++ src/test/java/org/eddy/generic/A.java
@@ -2,7 +2,7 @@ package org.eddy.generic;
 
 /**
  * 子类
- * Created by 轶 on 2017/2/26.
+ * Created by Justice-love on 2017/2/26.
  */
 public class A extends B {
 }
--- src/test/java/org/eddy/generic/B.java
+++ src/test/java/org/eddy/generic/B.java
@@ -2,7 +2,7 @@ package org.eddy.generic;
 
 /**
  * 父类
- * Created by 轶 on 2017/2/26.
+ * Created by Justice-love on 2017/2/26.
  */
 public class B {
 }
