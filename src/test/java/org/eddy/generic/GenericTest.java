53fee220cbeab1b6cfc8af83565545d180cf2755
eddy <eddyxu1213@126.com>
2017-02-26 15:27:04 +0800

    add

--- src/test/java/org/eddy/generic/GenericTest.java
+++ src/test/java/org/eddy/generic/GenericTest.java
@@ -17,5 +17,16 @@ public class GenericTest {
         B b = new B();
         list.add(a);//编译错误
         list.add(b);//编译错误
+        list.add(null); //正确
+    }
+
+    @Test
+    public void test2() {
+        List<? super B> list = new ArrayList<>();
+        A a = new A();
+        B b = new B();
+        list.add(new Object());//编译错误
+        list.add(b);//正确
+        list.add(a);//正确
     }
 }
